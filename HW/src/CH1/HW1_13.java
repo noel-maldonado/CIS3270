4234788dda8e9d5a9c9ffb94fe8ee4fdefdcbfa5
noel_maldonado <noel_maldonado@192.168.1.66>
2019-09-06 20:33:45 -0400

    first commit

---
+++ HW/src/CH1/HW1_13.java
@@ -0,0 +1,19 @@
+package CH1;
+
+public class HW1_13 {
+
+	public static void main(String[] args) {
+		System.out.println("3.4x + 50.2y = 44.5 \n2.1x +.55y =5.9");
+		System.out.print("x = ");
+		System.out.printf("%.4f",(double) (((44.5 * 0.55)-(50.2 * 5.9))/((3.4 * 0.55)-(50.2 * 2.1))));
+		System.out.println();
+		System.out.print("y = ");
+		System.out.printf("%.4f",(double) (((3.4 * 5.9)-(44.5 * 2.1))/((3.4 * 0.55)-(50.2 * 2.1))));
+		
+		
+		
+		
+		
+	}
+	
+}
